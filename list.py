4362c7d47577534b006ea0c29fb7d93e395b340e
leonardo.mendes@4linux.com.br <fabio.fugiyama@yahoo.com.br>
2018-06-26 20:53:11 -0300

    aula 2

--- list.py
+++ list.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 nome = ['fabio','carla','maria','joseee']
-print(nome[2])
+numeros = list(range(40,100))
+# print(nome[2])
+# nomes2 = nome[2:]
+# print(nomes2)
+# nome.append(['marta','goku']) insere no final da lista
+print(nome)
+nome.insert(0,'goku')#insere na posicao desejada
+print(nome)
+nome.pop()#retira o ultimo da lista
+print(nome)
+nome.remove('goku')#remove o item desejadoo
+print(nome)
+# print(nome[-1][1])
+#print(len(nome))
+print(numeros)
+
+if 43 in numeros:
+    print('sim')
+else:
+    print('nao')
+
+if 'fabio' in nome:
+    print('sim')
+else:
+    print('nao')
--- media.py
+++ media.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-nome = input('Digite o nome do aluno: \n')
-nota1 = int(input('Digite a primeira nota: \n'))
-nota2 = int(input('Digite a segunda nota: \n'))
+nome = str(input('Digite o nome do aluno: \n'))
+nota1 = float(input('Digite a primeira nota: \n'))
+nota2 = float(input('Digite a segunda nota: \n'))
 # nome = nome.title()
 # nome = nome.replace("a","@")
 # nome = nome.upper()
 # nome = nome.lower()
 media = (nota1 + nota2) / 2
-print('A media do {} foi de {}'.format(nome.title().replace("a","@").strip(),media))
+
+if media >= 7:
+    result = 'aprovado' #print('O aluno esta aprovado')
+else:
+    result = 'reprovado' # print('O aluno foi reprovado')
+
+print('A media do aluno {} é de {} e esta {}'.format(nome.title().strip(),media,result))
 print(nome)
