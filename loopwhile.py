fab472a496cc4703504f70eba599ac4fb1ddc11a
leonardo.mendes@4linux.com.br <fabio.fugiyama@yahoo.com.br>
2018-06-26 21:17:47 -0300

    aula2

---
+++ loopwhile.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+cont = 0
+# while cont >= 10:
+    # print(cont)
+    # cont += 1
+'''
+soma = 0
+while True:
+    num = int(input("Digite um numero ou 0 para sair: "))
+    if num == 0:
+        break
+    soma += num
+print('total: {}'.format(soma))
+'''
+
+
+
+nome = []
+
+while True:
+    x = input('Digite um nome ou 0 para sair: \n')
+    if x == '0':
+        break
+    nome.append(x)
+print(nome)
+
